d46d27c97d2a085987e22e0803a6828ac63e5df5
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2023-08-01 23:55:53 -0500

    finish header and navbar

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>This is the Home Page.</div>;
+};
+
+export default Home;
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+
+import Navbar from "@/components/navbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
